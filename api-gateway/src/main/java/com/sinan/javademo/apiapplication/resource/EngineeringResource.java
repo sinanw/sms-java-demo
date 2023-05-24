2a283719dbf193d2b8825221da5f15934c26ba08
sinanw <sinan.wannous@gmail.com>
2023-05-24 12:13:45 +0100

    Preparing APIs resources with ping testing endpoint. Task: SMS-4

---
+++ api-gateway/src/main/java/com/sinan/javademo/apiapplication/resource/EngineeringResource.java
@@ -0,0 +1,19 @@
+package com.sinan.javademo.apiapplication.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/engineering")
+@Produces(MediaType.TEXT_PLAIN)
+public class EngineeringResource {
+
+    public EngineeringResource(){}
+
+    @GET
+    @Path("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
