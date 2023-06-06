03f30149a64a5002f34dbd71eca5e6843dc7f812
sinanw <sinan.wannous@gmail.com>
2023-06-06 10:20:40 +0100

    Implementing test cases for engineering resource. Task: SMS-9

--- api-gateway/src/test/java/com/sinan/javademo/apiapplication/resource/EngineeringResourceTest.java
+++ api-gateway/src/test/java/com/sinan/javademo/apiapplication/resource/EngineeringResourceTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class EngineeringResourceTest{
+public class EngineeringResourceTest {
 
     @InjectMocks
     private EngineeringResource engineeringResource;
